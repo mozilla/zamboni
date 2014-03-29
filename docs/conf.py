991c31987a667425bea015384b7e4848ac658dfe
Mathieu Pillard <m@virgule.net>
2014-03-29 22:20:59 +0100

    Try to fix zamboni.readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -213,7 +213,12 @@ for key, val in intersphinx_mapping.items():
 # Root url where source files can be browsed online.
 src_base_url = 'http://github.com/mozilla/zamboni/tree/master/'
 
-import sphinx_rtd_theme
+# https://github.com/snide/sphinx_rtd_theme
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from
+# docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
