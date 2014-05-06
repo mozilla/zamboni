a88cbf355d8539012610aa659211fa26f7367aad
Allen Short <ashort@mozilla.com>
2014-05-05 19:40:41 -0500

    typo

--- migrations/780-custom-user.sql
+++ migrations/780-custom-user.sql
@@ -1,12 +1,11 @@
 ALTER TABLE api_access DROP FOREIGN KEY user_id_api;
-
 ALTER TABLE api_access
     CHANGE COLUMN user_id user_id int(11) unsigned NOT NULL,
     ADD CONSTRAINT user_id_api  FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE oauth_token DROP FOREIGN KEY user_id_refs_id_e213c7fc;
 ALTER TABLE oauth_token
-       CHANGE COLUMN user_id user_id int(11) unsigned NOT NULL,
+       CHANGE COLUMN user_id user_id int(11) unsigned DEFAULT NULL,
        ADD CONSTRAINT user_id_oauth_token FOREIGN KEY (user_id) REFERENCES users (id);
 
 ALTER TABLE users
