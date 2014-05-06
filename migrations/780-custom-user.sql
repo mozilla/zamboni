62296334ccb8bb0ffa16cac4ed59bad17dbd3c94
Allen Short <ashort@mozilla.com>
2014-05-05 19:20:18 -0500

    tweak migration

--- migrations/780-custom-user.sql
+++ migrations/780-custom-user.sql
@@ -1,10 +1,11 @@
+ALTER TABLE api_access DROP FOREIGN KEY user_id_api;
+
 ALTER TABLE api_access
-    DROP FOREIGN KEY user_id_api,
     CHANGE COLUMN user_id user_id int(11) unsigned NOT NULL,
     ADD CONSTRAINT user_id_api  FOREIGN KEY (user_id) REFERENCES users (id);
 
+ALTER TABLE oauth_token DROP FOREIGN KEY user_id_refs_id_e213c7fc;
 ALTER TABLE oauth_token
-       DROP FOREIGN KEY user_id_refs_id_e213c7fc,
        CHANGE COLUMN user_id user_id int(11) unsigned NOT NULL,
        ADD CONSTRAINT user_id_oauth_token FOREIGN KEY (user_id) REFERENCES users (id);
 
