870dc6acbe3aa8befc7b4092afcf51163a191d54
Andy McKay <amckay@mozilla.com>
2014-06-04 17:20:43 -0700

    fix validate_settings

--- lib/tests/test_utils_.py
+++ lib/tests/test_utils_.py
@@ -7,12 +7,17 @@ from lib.utils import validate_settings
 class TestValidate(TestCase):
 
     def test_secret_key(self):
-        with self.settings(DEBUG=True, SECRET_KEY='please change this'):
+        with self.settings(DEBUG=True, IN_TEST_SUITE=False,
+                           SECRET_KEY='please change this'):
             validate_settings()
 
-        with self.settings(DEBUG=False, SECRET_KEY='please change this'):
+        with self.settings(DEBUG=False, IN_TEST_SUITE=False,
+                           SECRET_KEY='please change this'):
             with self.assertRaises(ImproperlyConfigured):
                 validate_settings()
 
-        with self.settings(DEBUG=False, SECRET_KEY='so changed'):
+        with self.settings(DEBUG=False, IN_TEST_SUITE=False,
+                           SECRET_KEY='so changed',
+                           SESSION_COOKIE_SECURE=True,
+                           APP_PURCHASE_SECRET='so changed'):
             validate_settings()
