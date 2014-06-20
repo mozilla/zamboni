8a18e3ecf1a0928a8ed8c82fa4b3b27698f4ebfa
Christopher Van <cvan@mozilla.com>
2014-06-20 11:09:40 -0700

    Revert "do not yet run migration for model changes to `Contribution` and `Addon` (bug 1026281)"
    This reverts commit 35074a3bde814a89e63202817eb7aaa8949002b0.

--- migrations/814-rm-amo-contributions.sql
+++ migrations/814-rm-amo-contributions.sql
@@ -1,23 +1,20 @@
--- Because this migration can take a while, we will run these manually on
--- prod after the 2014-07-01 push.
+-- remove add-on-specific fields from `Contribution` model (bug 1026281)
 
--- -- remove add-on-specific fields from `Contribution` model (bug 1026281)
+-- `Contribution.post_data`
+alter table stats_contributions drop column post_data;
 
--- -- `Contribution.post_data`
--- alter table stats_contributions drop column post_data;
+-- `Contribution.charity`
+alter table stats_contributions drop foreign key stats_contributions_ibfk_2;
+alter table stats_contributions drop column charity_id;
 
--- -- `Contribution.charity`
--- alter table stats_contributions drop foreign key stats_contributions_ibfk_2;
--- alter table stats_contributions drop column charity_id;
+alter table addons drop foreign key addons_ibfk_15;
+alter table addons drop column charity_id;
 
--- alter table addons drop foreign key addons_ibfk_15;
--- alter table addons drop column charity_id;
+drop table charities;
 
--- drop table charities;
-
--- -- `Contribution.annoying`
--- -- `Contribution.is_suggested`
--- -- `Contribution.suggested_amount`
--- alter table stats_contributions drop column annoying,
---                                 drop column is_suggested,
---                                 drop column suggested_amount;
+-- `Contribution.annoying`
+-- `Contribution.is_suggested`
+-- `Contribution.suggested_amount`
+alter table stats_contributions drop column annoying,
+                                drop column is_suggested,
+                                drop column suggested_amount;
