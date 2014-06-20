aa81175e6bc5ccfb2d2c6094aaa61f2198d89b69
Kevin Ngo <ngoke@mozilla.com>
2014-06-20 14:20:18 -0700

    feed shelf publish view

--- mkt/api/v2/urls.py
+++ mkt/api/v2/urls.py
@@ -2,44 +2,43 @@ from django.conf.urls import include, patterns, url
 
 from rest_framework.routers import SimpleRouter
 
+import mkt.feed.views as views
 from mkt.api.base import SubRouterWithFormat
 from mkt.api.v1.urls import urlpatterns as v1_urls
 from mkt.api.views import endpoint_removed
-from mkt.feed.views import (FeedAppImageViewSet, FeedAppViewSet,
-                            FeedBrandViewSet, FeedBuilderView,
-                            FeedCollectionImageViewSet, FeedCollectionViewSet,
-                            FeedElementSearchView, FeedItemViewSet,
-                            FeedShelfViewSet, FeedShelfImageViewSet)
 
 
 feed = SimpleRouter()
-feed.register(r'apps', FeedAppViewSet, base_name='feedapps')
-feed.register(r'brands', FeedBrandViewSet, base_name='feedbrands')
-feed.register(r'collections', FeedCollectionViewSet,
+feed.register(r'apps', views.FeedAppViewSet, base_name='feedapps')
+feed.register(r'brands', views.FeedBrandViewSet, base_name='feedbrands')
+feed.register(r'collections', views.FeedCollectionViewSet,
               base_name='feedcollections')
-feed.register(r'items', FeedItemViewSet, base_name='feeditems')
-feed.register(r'shelves', FeedShelfViewSet, base_name='feedshelves')
+feed.register(r'items', views.FeedItemViewSet, base_name='feeditems')
+feed.register(r'shelves', views.FeedShelfViewSet, base_name='feedshelves')
 
 subfeedapp = SubRouterWithFormat()
-subfeedapp.register('image', FeedAppImageViewSet,
+subfeedapp.register('image', views.FeedAppImageViewSet,
                     base_name='feed-app-image')
 
 subfeedcollection = SubRouterWithFormat()
-subfeedcollection.register('image', FeedCollectionImageViewSet,
+subfeedcollection.register('image', views.FeedCollectionImageViewSet,
                     base_name='feed-collection-image')
 
 subfeedshelf = SubRouterWithFormat()
-subfeedshelf.register('image', FeedShelfImageViewSet,
+subfeedshelf.register('image', views.FeedShelfImageViewSet,
                       base_name='feed-shelf-image')
 
 urlpatterns = patterns('',
     url(r'^rocketfuel/collections/.*', endpoint_removed),
-    url(r'^feed/builder/$', FeedBuilderView.as_view(),
+    url(r'^feed/builder/$', views.FeedBuilderView.as_view(),
         name='feed.builder'),
-    url(r'^feed/elements/search/$', FeedElementSearchView.as_view(),
+    url(r'^feed/elements/search/$', views.FeedElementSearchView.as_view(),
         name='feed.element-search'),
     url(r'^feed/', include(feed.urls)),
     url(r'^feed/apps/', include(subfeedapp.urls)),
     url(r'^feed/collections/', include(subfeedcollection.urls)),
     url(r'^feed/shelves/', include(subfeedshelf.urls)),
+    url(r'^feed/shelves/(?P<pk>[^/.]+)/publish',
+        views.FeedShelfPublishView.as_view(),
+        name='feed-shelf-publish'),
 ) + v1_urls
