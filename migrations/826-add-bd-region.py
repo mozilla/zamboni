91d10a1c4d5970ba1b5a5c8d91b228e9e2c67712
Rob Hudson <chudson@mozilla.com>
2014-07-29 10:49:39 -0700

    Removed `send_new_region_emails` from migration
    We will run this on prod only manually.

--- migrations/826-add-bd-region.py
+++ migrations/826-add-bd-region.py
@@ -1,7 +1,6 @@
 from mkt.constants import regions
-from mkt.developers.cron import exclude_new_region, send_new_region_emails
+from mkt.developers.cron import exclude_new_region
 
 
 def run():
     exclude_new_region([regions.BD])
-    send_new_region_emails([regions.BD])
