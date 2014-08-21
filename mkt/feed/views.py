cc64a58989526460dc3b21fddf6a3bb35e4fa720
Kevin Ngo <ngoke@mozilla.com>
2014-08-21 14:35:31 -0700

    oops, we need device_type to be sent to feed (bug 1057068)

--- mkt/feed/views.py
+++ mkt/feed/views.py
@@ -520,7 +520,7 @@ class BaseFeedESView(CORSMixin, APIView):
         """
         apps = feed_element.get('apps') or [feed_element['app']]
         for app in apps:
-            for field in ('device_types', 'regions', 'status'):
+            for field in ('regions', 'status'):
                 if field in app:
                     del app[field]
         return feed_element
@@ -757,10 +757,10 @@ class FeedElementGetView(BaseFeedESView):
             'request': request
         }).data
 
-        # Filter data. If None of the apps are compatible, show everything
-        # non-filtered since they must have navigated to the feed element
-        # manually.
-        data = self.filter_apps_feed_element(request, dict(data)) or data
+        # Filter data. If None of the apps are compatible, only run the
+        # public apps filter.
+        data = (self.filter_apps_feed_element(request, dict(data)) or
+                self._filter(amo.STATUS_PUBLIC, 'status', feed_element))
 
         return response.Response(data, status=status.HTTP_200_OK)
 
