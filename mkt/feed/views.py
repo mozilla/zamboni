bf51ab2a0a10a7ea6e24803d738ec2b5ad827f37
Kevin Ngo <ngoke@mozilla.com>
2014-08-27 20:46:10 -0700

    fix feed tests

--- mkt/feed/views.py
+++ mkt/feed/views.py
@@ -53,7 +53,7 @@ class ImageURLUploadMixin(viewsets.ModelViewSet):
             # save img in appropriate directory.
             obj._background_image_upload = img
             obj.image_hash = hash_
-        elif getattr(obj, 'type') == feed.COLLECTION_PROMO:
+        elif hasattr(obj, 'type') and obj.type == feed.COLLECTION_PROMO:
             # Remove background images for promo collections.
             obj.image_hash = ''
         return super(ImageURLUploadMixin, self).pre_save(obj)
