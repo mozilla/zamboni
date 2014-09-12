a507494198f322a368a640bbb1d45f704727b98d
Andy McKay <amckay@mozilla.com>
2014-09-12 12:07:32 -0700

    ensure that thumbnails have correct permission (bug 928242)

--- lib/video/dummy.py
+++ lib/video/dummy.py
@@ -1,3 +1,5 @@
+import tempfile
+
 from .utils import VideoBase
 
 
@@ -7,3 +9,12 @@ class Video(VideoBase):
     @classmethod
     def library_available(cls):
         return True
+
+    def is_valid(self):
+        return self.filename.endswith('.webm')
+
+    def get_encoded(self, size):
+        return tempfile.mkstemp()[1]
+
+    def get_screenshot(self, size):
+        return tempfile.mkstemp()[1]
