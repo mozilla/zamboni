11bc3a7a55e387a316e0530381ab4c9a0633efb3
Mark Striemer <mstriemer@mozilla.com>
2014-09-16 10:57:54 -0500

    Firefox Accounts banner setup (bug 1052864)
    Create the waffle and remove aside#incompatibility-banner.

---
+++ migrations/848-waffle-switch-fx-accounts-migration.sql
@@ -0,0 +1,6 @@
+INSERT INTO waffle_switch (name, active, note, created, modified)
+    VALUES ('fx-accounts-migration',
+            0,
+            'Enable the Firefox Accounts migration.',
+            NOW(),
+            NOW());
