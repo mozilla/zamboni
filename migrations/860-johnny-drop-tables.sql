7e456315f52d9438d8dad825f700f68732f17b23
Rob Hudson <chudson@mozilla.com>
2014-09-30 13:09:05 -0700

    Remove unused tables (bug 1070007)

---
+++ migrations/860-johnny-drop-tables.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS auth_message;
+DROP TABLE IF EXISTS charities;
+DROP TABLE IF EXISTS django_session_old;
+DROP TABLE IF EXISTS django_site;
+DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS tag_stat;
