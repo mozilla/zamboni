94ffd16b29ddf461e06983a20fef6e13d9082e18
Rob Hudson <robhudson@mozilla.com>
2014-10-14 15:10:05 -0700

    Consolidate paginator helpers (bug 1066692)

--- apps/amo/helpers.py
+++ apps/amo/helpers.py
@@ -1,12 +1,8 @@
 import json as jsonlib
 from urlparse import urljoin
 
-from django.conf import settings
-from django.core.exceptions import ObjectDoesNotExist
 from django.core.urlresolvers import reverse
 from django.template import defaultfilters
-from django.utils import translation
-from django.utils.encoding import smart_unicode
 
 import jinja2
 import six
@@ -32,50 +28,9 @@ register.function(utils.randslice)
 
 
 @register.filter
-def paginator(pager):
-    return Paginator(pager).render()
-
-
-@register.filter
 def impala_paginator(pager):
     t = env.get_template('amo/impala/paginator.html')
     return jinja2.Markup(t.render({'pager': pager}))
-
-
-class Paginator(object):
-
-    def __init__(self, pager):
-        self.pager = pager
-
-        self.max = 10
-        self.span = (self.max - 1) / 2
-
-        self.page = pager.number
-        self.num_pages = pager.paginator.num_pages
-        self.count = pager.paginator.count
-
-        pager.page_range = self.range()
-        pager.dotted_upper = self.num_pages not in pager.page_range
-        pager.dotted_lower = 1 not in pager.page_range
-
-    def range(self):
-        """Return a list of page numbers to show in the paginator."""
-        page, total, span = self.page, self.num_pages, self.span
-        if total < self.max:
-            lower, upper = 0, total
-        elif page < span + 1:
-            lower, upper = 0, span * 2
-        elif page > total - span:
-            lower, upper = total - span * 2, total
-        else:
-            lower, upper = page - span, page + span - 1
-        return range(max(lower + 1, 1), min(total, upper) + 1)
-
-    def render(self):
-        c = {'pager': self.pager, 'num_pages': self.num_pages,
-             'count': self.count}
-        t = env.get_template('amo/paginator.html').render(c)
-        return jinja2.Markup(t)
 
 
 @register.function
