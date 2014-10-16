ceb0e457e619d2e8c8700727f91e19097de30abd
Jason Thomas <jason@lithiumfox.com>
2014-10-16 14:14:09 -0400

    Add RECOMMENDATIONS settings to -dev.

--- sites/dev/settings_mkt.py
+++ sites/dev/settings_mkt.py
@@ -178,3 +178,6 @@ FXA_AUTH_DOMAIN = getattr(private_mkt, 'FXA_AUTH_DOMAIN', '')
 FXA_OAUTH_URL = getattr(private_mkt, 'FXA_OAUTH_URL', '')
 FXA_CLIENT_ID = getattr(private_mkt, 'FXA_CLIENT_ID', '')
 FXA_CLIENT_SECRET = getattr(private_mkt, 'FXA_CLIENT_SECRET', '')
+
+RECOMMENDATIONS_API_URL = 'https://recommend-dev.allizom.org'
+RECOMMENDATIONS_ENABLED = True
