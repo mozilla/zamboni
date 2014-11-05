cbd0b35bea9a80d3739eba3eb56195d198fa3e9b
Allen Short <ashort@mozilla.com>
2014-11-04 23:39:47 -0800

    Only send FxA mail to users from after guillotine split (bug 1093845)

--- mkt/users/tests/test_commands.py
+++ mkt/users/tests/test_commands.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from nose.tools import eq_
 
 import amo.tests
@@ -15,13 +16,23 @@ class TestCommand(amo.tests.TestCase):
         self.developer = UserProfile.objects.get(pk=31337)
 
     def test_user(self):
+        self.user.update(last_login_attempt=datetime(2014, 5, 1))
         eq_(get_user_ids(False), [999L])
         self.user.update(source=LOGIN_SOURCE_FXA)
         eq_(get_user_ids(False), [])
 
+    def test_stale_user(self):
+        self.user.update(last_login_attempt=datetime(2014, 4, 30))
+        eq_(get_user_ids(False), [])
+
     def test_developers(self):
+        self.developer.update(last_login_attempt=datetime(2014, 5, 1))
         eq_(get_user_ids(True), [31337L])
         self.developer.update(source=LOGIN_SOURCE_FXA)
+        eq_(get_user_ids(True), [])
+
+    def test_stale_developers(self):
+        self.developer.update(last_login_attempt=datetime(2014, 4, 30))
         eq_(get_user_ids(True), [])
 
     def test_user_filtered_correctly(self):
@@ -30,5 +41,7 @@ class TestCommand(amo.tests.TestCase):
         out for an email sent to users. We do this by setting the FxA source on
         the developer before getting developers.
         """
+        self.user.update(last_login_attempt=datetime(2014, 5, 1))
+        self.developer.update(last_login_attempt=datetime(2014, 5, 1))
         self.developer.update(source=LOGIN_SOURCE_FXA)
         eq_(get_user_ids(False), [999L])
