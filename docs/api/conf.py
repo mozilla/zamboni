55d5010967a0ca1d7e08366339ecdb479ea27f87
Kevin Ngo <ngoke@mozilla.com>
2014-11-25 04:12:28 -0800

    fix api doc title, bump to v2

--- docs/api/conf.py
+++ docs/api/conf.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.abspath('../..'))
 from docs.conf import *  # noqa
 
 
-project = u'API v1'
-version = release = '1.0'  # Should correspond to the API version number
+project = u'Marketplace API'
+version = release = '2.0'  # Should correspond to the API version number
 
 intersphinx_mapping = {}
