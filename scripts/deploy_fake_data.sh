bea170afd9697a0a2253a309764129e869fe5042
Allen Short <ashort@mozilla.com>
2015-02-09 11:30:54 -0800

    script for creating new fake-data db

---
+++ scripts/deploy_fake_data.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+python manage.py syncdb --noinput
+python manage.py loaddata init
+schematic migrations/ --fake
+python manage.py reindex
+python manage.py generate_apps_from_spec data/apps/test_apps.json
