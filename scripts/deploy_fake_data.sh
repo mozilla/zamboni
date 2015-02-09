f44afa89793248bc6d027f00cdc410a1bac43b13
Allen Short <ashort@mozilla.com>
2015-02-09 15:04:45 -0800

    run reindex last when prepping fake data

--- scripts/deploy_fake_data.sh
+++ scripts/deploy_fake_data.sh
@@ -2,5 +2,5 @@
 python manage.py syncdb --noinput
 python manage.py loaddata init
 schematic migrations/ --fake
-python manage.py reindex
 python manage.py generate_apps_from_spec data/apps/test_apps.json
+python manage.py reindex
