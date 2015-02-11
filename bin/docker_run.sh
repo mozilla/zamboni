d5a279ef2159a492a68c7dfa11fd06ee04c602ff
Allen Short <ashort@mozilla.com>
2015-02-11 13:58:18 -0600

    Create fake apps when setting up zamboni docker instance. (bug 1065602)

--- bin/docker_run.sh
+++ bin/docker_run.sh
@@ -14,6 +14,8 @@ if [ $? -ne 0 ]; then
     python manage.py loaddata init
     echo "Jumping migrations forward to the most recent."
     schematic migrations/ --fake
+    echo "Creating fake apps."
+    python manage.py generate_apps_from_spec data/apps/test_apps.json
     echo "Creating the initial index"
     python manage.py reindex
 fi
