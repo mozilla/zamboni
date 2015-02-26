cfe1e9d1accc8f03134c33ae509e58dca258c951
Mathieu Pillard <mpillard@mozilla.com>
2015-02-26 15:54:38 +0100

    Fix migration (bug 1131717)
    Avoids this:
    ERROR 1834 (HY000) at line 3: Cannot delete rows from table which is parent in a foreign key constraint
    'additional_review_reviewer_id_refs_users_id' of table 'additional_review'

--- migrations/897-remove-username-and-extra-fields.sql
+++ migrations/897-remove-username-and-extra-fields.sql
@@ -1,3 +1,7 @@
+-- MySQL chokes on this migration if FK checks are not disabled, even though
+-- no FK relies on any of the modified fields :(
+SET FOREIGN_KEY_CHECKS=0;
+
 -- username does not exist anymore.
 ALTER TABLE `users` CHANGE `username` `fxa_uid`  varchar(255);
 -- We no longer log login attempts, since they fail at FxA level, before our code.
@@ -7,3 +11,5 @@ ALTER TABLE `users` DROP COLUMN `failed_login_attempts`;
 -- We no longer need the password field, but keep it for now because it's
 -- easier. This migration reverts it to its default length.
 ALTER TABLE `users` MODIFY `password` varchar(128);
+
+SET FOREIGN_KEY_CHECKS=1;
