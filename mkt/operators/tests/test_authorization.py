mozilla/zamboni

commit 6c0a2917e0c2ac584594f54531d4a8e8ef04b119

Add a search view without region filtering for curation tools (bug 1136460)
Also remove trailing garbage after search API urls, to end possible confusion
between /api/v1/apps/search/suggest and /api/v1/apps/search/suggest/. The former
is no longer tolerated (was never documented, only used by accident in transonic)

mkt/search/views.py
@@ -90,7 +90,8 @@ class SuggestionsView(SearchView):
 class NonPublicSearchView(SearchView):
     """
     A search view that allows searching for apps with non-public statuses
-    protected behind a permission class.
+    protected behind a permission class. Region exclusions still affects
+    results.
 
     """
     authentication_classes = [RestSharedSecretAuthentication,
@@ -98,6 +99,25 @@ class NonPublicSearchView(SearchView):
     permission_classes = [GroupPermission('Feed', 'Curate')]
     filter_backends = [SearchQueryFilter, PublicSearchFormFilter,
                        ValidAppsFilter, DeviceTypeFilter, RegionFilter,
+                       ProfileFilter, SortingFilter]
+
+
+class NoRegionSearchView(SearchView):
+    """
+    A search view that allows searching for public apps regardless of region
+    exclusions, protected behind a permission class.
+
+    A special class is needed because when RegionFilter is included, as it is
+    in the default SearchView, it will always use whatever region was set on
+    the request, and we default to setting restofworld when no region is
+    passed.
+
+    """
+    authentication_classes = [RestSharedSecretAuthentication,
+                              RestOAuthAuthentication]
+    permission_classes = [GroupPermission('Feed', 'Curate')]
+    filter_backends = [SearchQueryFilter, PublicSearchFormFilter,
+                       PublicAppsFilter, DeviceTypeFilter,
                        ProfileFilter, SortingFilter]
 
 

mkt/search/tests/test_views.py
@@ -1050,6 +1050,72 @@ class TestNonPublicSearchView(RestOAuth, ESTestCase):
             eq_(res.status_code, 200)
             eq_(len(res.json['objects']), 0)
 
+    def test_not_finds_excluded_region(self):
+        self.app2.addonexcludedregion.create(region=mkt.regions.USA.id)
+        self.reindex(Webapp, 'webapp')
+        res = self.client.get(self.url, data={
+            'q': 'second', 'lang': 'en-US', 'region': 'us'
+        })
+        eq_(res.status_code, 200)
+        eq_(len(res.json['objects']), 0)
+
+
+@patch.object(settings, 'SITE_URL', 'http://testserver')
+class TestNoRegionSearchView(RestOAuth, ESTestCase):
+    fixtures = fixture('user_2519', 'webapp_337141')
+
+    def setUp(self):
+        super(TestNoRegionSearchView, self).setUp()
+        self.url = reverse('api-v2:no-region-search-api')
+        self.refresh('webapp')
+        self.app1 = Webapp.objects.get(pk=337141)
+        self.app2 = app_factory(name=u'Second âpp',
+                                description=u'Second dèsc' * 25,
+                                icon_type='image/png',
+                                created=self.days_ago(3))
+        # Exclude the app in the region we are going to send. It should not
+        # matter.
+        self.app2.addonexcludedregion.create(region=mkt.regions.USA.id)
+        self.grant_permission(self.profile, 'Feed:Curate')
+        self.reindex(Webapp, 'webapp')
+
+    def tearDown(self):
+        # Cleanup to remove these from the index.
+        self.app1.delete()
+        self.app2.delete()
+        unindex_webapps([self.app1.id, self.app2.id])
+
+    def test_anonymous(self):
+        res = self.anon.get(self.url, data={'q': 'second'})
+        eq_(res.status_code, 403)
+
+    def test_no_permission(self):
+        GroupUser.objects.filter(user=self.profile).delete()
+        res = self.client.get(self.url, data={'q': 'second', 'region': 'us'})
+        eq_(res.status_code, 403)
+
+    def test_with_permission(self):
+        res = self.client.get(self.url, data={
+            'q': 'second', 'lang': 'en-US', 'region': 'us'
+        })
+        eq_(res.status_code, 200)
+        objs = res.json['objects']
+        eq_(len(objs), 1)
+        eq_(objs[0]['name'], self.app2.name)
+
+    def test_not_finds_invalid_statuses(self):
+        for status in [mkt.STATUS_PENDING, mkt.STATUS_APPROVED,
+                       mkt.STATUS_UNLISTED, mkt.STATUS_DISABLED,
+                       mkt.STATUS_DELETED, mkt.STATUS_REJECTED,
+                       mkt.STATUS_BLOCKED]:
+            self.app2.update(status=status)
+            self.refresh('webapp')
+            res = self.client.get(self.url, data={
+                'q': 'second', 'lang': 'en-US', 'region': 'us'
+            })
+            eq_(res.status_code, 200)
+            eq_(len(res.json['objects']), 0)
+
 
 class TestRocketbarView(ESTestCase):
     fixtures = fixture('user_2519', 'webapp_337141')

mkt/api/v1/urls.py
@@ -53,13 +53,13 @@ urlpatterns = patterns(
     url('', include('mkt.fireplace.urls')),
     url(r'^apps/', include(apps.urls)),
     url(r'^apps/app/', include(subapps.urls)),
-    url(r'^apps/search/featured/', FeaturedSearchView.as_view(),
+    url(r'^apps/search/featured/$', FeaturedSearchView.as_view(),
         name='featured-search-api'),
-    url(r'^apps/search/suggest/', SuggestionsView.as_view(),
+    url(r'^apps/search/suggest/$', SuggestionsView.as_view(),
         name='suggestions-search-api'),
-    url(r'^apps/search/rocketbar/', RocketbarView.as_view(),
+    url(r'^apps/search/rocketbar/$', RocketbarView.as_view(),
         name='rocketbar-search-api'),
-    url(r'^apps/search/', SearchView.as_view(), name='search-api'),
+    url(r'^apps/search/$', SearchView.as_view(), name='search-api'),
     url(r'^services/', include(services.urls)),
     url(r'^services/config/site/', site_config, name='site-config'),
     url(r'^fireplace/report_error/\d*', error_reporter, name='error-reporter'),

mkt/api/v2/urls.py
@@ -11,7 +11,8 @@ from mkt.comm.views import CommAppListView, ThreadViewSetV2
 from mkt.langpacks.views import LangPackViewSet
 from mkt.operators.views import OperatorPermissionViewSet
 from mkt.recommendations.views import RecommendationView
-from mkt.search.views import NonPublicSearchView, RocketbarViewV2
+from mkt.search.views import (NonPublicSearchView, NoRegionSearchView,
+                              RocketbarViewV2)
 
 
 feed = SimpleRouter()
@@ -55,10 +56,13 @@ urlpatterns = patterns(
 
     url(r'^apps/recommend/$', RecommendationView.as_view(),
         name='apps-recommend'),
-    url(r'^apps/search/rocketbar/', RocketbarViewV2.as_view(),
+    url(r'^apps/search/rocketbar/$', RocketbarViewV2.as_view(),
         name='rocketbar-search-api'),
-    url(r'^apps/search/non-public/', NonPublicSearchView.as_view(),
+    url(r'^apps/search/non-public/$', NonPublicSearchView.as_view(),
         name='non-public-search-api'),
+    url(r'^apps/search/no-region/$',
+        NoRegionSearchView.as_view(),
+        name='no-region-search-api'),
 
     url(r'^comm/app/%s' % mkt.APP_SLUG,
         CommAppListView.as_view({'get': 'list'}), name='comm-app-list'),

commit e7a04515d0047c60f21e3a1ace0568d9b4e50eb2

Allow users with operator permissions to access NoRegionSearchView (regression from 6c0a291; bug 1144835)

mkt/operators/authorization.py
@@ -1,0 +1,11 @@
+from mkt.operators.models import OperatorPermission
+
+from rest_framework import permissions
+
+
+class IsOperatorPermission(permissions.BasePermission):
+    """
+    Pass if the user has operator permissions for any carrier/region pair.
+    """
+    def has_permission(self, request, view):
+        return OperatorPermission.user_is_operator(request.user)

mkt/operators/models.py
@@ -34,3 +34,15 @@ class OperatorPermission(ModelBase):
         """
         return cls.objects.get(
             user=user, carrier=carrier, region=region).exists()
+
+    @classmethod
+    def user_is_operator(cls, user):
+        """
+        Returns a boolean indicating whether the passed user has operator
+        permissions for any carrier/region pair.
+
+        Example:
+        is_operator = OperatorPermission.user_is_operator(request.user)
+        """
+        return (user.is_authenticated() and
+                cls.objects.filter(user=user).exists())

mkt/search/views.py
@@ -11,9 +11,10 @@ from rest_framework.response import Response
 
 from mkt.api.authentication import (RestOAuthAuthentication,
                                     RestSharedSecretAuthentication)
-from mkt.api.authorization import GroupPermission
+from mkt.api.authorization import AnyOf, GroupPermission
 from mkt.api.base import CORSMixin, MarketplaceView
 from mkt.api.paginator import ESPaginator
+from mkt.operators.authorization import IsOperatorPermission
 from mkt.search.forms import ApiSearchForm
 from mkt.search.filters import (DeviceTypeFilter, ProfileFilter,
                                 PublicAppsFilter, PublicSearchFormFilter,
@@ -124,7 +125,8 @@ class NoRegionSearchView(SearchView):
     """
     authentication_classes = [RestSharedSecretAuthentication,
                               RestOAuthAuthentication]
-    permission_classes = [GroupPermission('Feed', 'Curate')]
+    permission_classes = [AnyOf(GroupPermission('Feed', 'Curate'),
+                                IsOperatorPermission)]
     filter_backends = [SearchQueryFilter, PublicSearchFormFilter,
                        PublicAppsFilter, DeviceTypeFilter,
                        ProfileFilter, SortingFilter]

mkt/operators/tests/test_authorization.py
@@ -1,0 +1,40 @@
+from nose.tools import ok_
+from rest_framework.generics import GenericAPIView
+
+from django.contrib.auth.models import AnonymousUser
+from django.test.client import RequestFactory
+
+from mkt.access.middleware import ACLMiddleware
+from mkt.operators.authorization import IsOperatorPermission
+from mkt.operators.models import OperatorPermission
+from mkt.site.fixtures import fixture
+from mkt.site.tests import TestCase
+from mkt.users.models import UserProfile
+
+
+class TestIsOperatorPermission(TestCase):
+    fixtures = fixture('user_2519')
+
+    def setUp(self):
+        super(TestIsOperatorPermission, self).setUp()
+        self.auth = IsOperatorPermission()
+        self.user = UserProfile.objects.get(pk=2519)
+        self.profile = self.user
+        self.view = GenericAPIView()
+
+    def is_authorized(self, anon=False):
+        request = RequestFactory().get('/')
+        request.user = AnonymousUser() if anon else self.user
+        ACLMiddleware().process_request(request)
+        return self.auth.has_permission(request, self.view)
+
+    def test_anon(self):
+        ok_(not self.is_authorized(anon=True))
+
+    def test_auth_no_perm(self):
+        ok_(not self.is_authorized())
+
+    def test_auth_with_perm(self):
+        OperatorPermission.objects.create(user=self.profile, region=1,
+                                          carrier=8)
+        ok_(self.is_authorized())

mkt/search/tests/test_views.py
@@ -19,6 +19,7 @@ from mkt.api.tests.test_oauth import RestOAuth, RestOAuthClient
 from mkt.constants import regions
 from mkt.constants.applications import DEVICE_CHOICES_IDS
 from mkt.constants.features import FeatureProfile
+from mkt.operators.models import OperatorPermission
 from mkt.regions.middleware import RegionMiddleware
 from mkt.search.filters import SortingFilter
 from mkt.search.views import SearchView
@@ -1095,6 +1096,18 @@ class TestNoRegionSearchView(RestOAuth, ESTestCase):
         eq_(res.status_code, 403)
 
     def test_with_permission(self):
+        res = self.client.get(self.url, data={
+            'q': 'second', 'lang': 'en-US', 'region': 'us'
+        })
+        eq_(res.status_code, 200)
+        objs = res.json['objects']
+        eq_(len(objs), 1)
+        eq_(objs[0]['name'], self.app2.name)
+
+    def test_with_operator_permission(self):
+        GroupUser.objects.filter(user=self.profile).delete()
+        OperatorPermission.objects.create(user=self.profile, carrier=1,
+                                          region=8)
         res = self.client.get(self.url, data={
             'q': 'second', 'lang': 'en-US', 'region': 'us'
         })