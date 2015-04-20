f27855491d3f3ae352b87741a19ad45ca48cd5f1
Wil Clouser <clouserw@micropipes.com>
2015-04-20 11:08:29 -0700

    enable ee, ff, ig, wo, yo locales; bug 1154915

--- sites/stage/settings_mkt.py
+++ sites/stage/settings_mkt.py
@@ -85,8 +85,7 @@ if getattr(private_mkt, 'LOAD_TESTING', False):
     # secrets used for them
     API_PASSWORD = getattr(private_mkt, 'API_PASSWORD', FAKEAUTH_TOKEN)
 
-AMO_LANGUAGES = AMO_LANGUAGES + ('dbg', 'rtl', 'ar', 'ee', 'ff', 'ha', 'ig',
-                                 'ln', 'sw', 'tl', 'wo', 'yo')
+AMO_LANGUAGES = AMO_LANGUAGES + ('dbg', 'rtl', 'ar', 'ha', 'ln', 'sw', 'tl')
 LANGUAGES = lazy(langs, dict)(AMO_LANGUAGES)
 LANGUAGE_URL_MAP = dict([(i.lower(), i) for i in AMO_LANGUAGES])
 
