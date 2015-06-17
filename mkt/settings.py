da225cced3b46127a34d8715f1d3710e808dc94b
Wil Clouser <clouserw@micropipes.com>
2015-06-17 13:07:20 -0700

    add Greek; bug 1173891

--- mkt/settings.py
+++ mkt/settings.py
@@ -495,7 +495,7 @@ ALLOW_SELF_REVIEWS = True
 # A smaller range of languages for the Marketplace.
 AMO_LANGUAGES = (
     'af', 'ar', 'bg', 'bn-BD', 'bn-IN', 'ca', 'cs', 'cy', 'da', 'de', 'dsb',
-    'ee', 'en-US', 'es', 'eu', 'ff', 'fr', 'fy', 'ga-IE', 'ha', 'hsb',
+    'ee', 'el', 'en-US', 'es', 'eu', 'ff', 'fr', 'fy', 'ga-IE', 'ha', 'hsb',
     'hu', 'id', 'ig', 'it', 'ja', 'ko', 'nb-NO', 'nl', 'pl',
     'pt-BR', 'ru', 'sk', 'sq', 'sr', 'sr-Latn', 'sv-SE', 'sw', 'tr',
     'uk', 'wo', 'xh', 'yo', 'zh-CN', 'zh-TW', 'zu',
