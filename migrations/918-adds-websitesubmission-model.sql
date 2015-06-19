5f059ee92a044afb9784c5801eab1410e44370b5
Chuck Harmston <chuck@chuckharmston.com>
2015-06-19 16:57:29 -0600

    Fixes SQL errors in migration 918.

--- migrations/918-adds-websitesubmission-model.sql
+++ migrations/918-adds-websitesubmission-model.sql
@@ -1,19 +1,19 @@
-CREATE TABLE `websites_websitesubmission_keywords` (
+CREATE TABLE `websites_websitesubmission_keywords_tag_id` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `website_id` integer UNIQUE,
-    `tag_id` integer unsigned NOT NULL,
-    UNIQUE (`website_id`, `tag_id`)
+    `websitesubmission_id` integer NOT NULL,
+    `tag_id` integer NOT NULL,
+    UNIQUE (`websitesubmission_id`, `tag_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `websites_websitesubmission_keywords` ADD CONSTRAINT `websites_websitesubmission_keywords_tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`);
+ALTER TABLE `websites_websitesubmission_keywords` ADD CONSTRAINT `websites_websitesubmission_keywords__tag_id` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`);
 
 CREATE TABLE `websites_websitesubmission` (
     `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
     `created` datetime NOT NULL,
     `modified` datetime NOT NULL,
     `date_approved` datetime NULL,
-    `name` integer unsigned UNIQUE,
-    `description` integer unsigned UNIQUE,
+    `name` integer UNIQUE,
+    `description` integer UNIQUE,
     `categories` longtext NOT NULL,
     `detected_icon` varchar(255) NOT NULL,
     `icon_type` varchar(25) NOT NULL,
@@ -21,15 +21,15 @@ CREATE TABLE `websites_websitesubmission` (
     `url` varchar(255) NOT NULL,
     `canonical_url` varchar(255),
     `works_well` integer unsigned UNIQUE,
-    `submitter_id` integer unsigned UNIQUE,
+    `submitter_id` integer UNIQUE,
     `public_credit` bool NOT NULL DEFAULT FALSE,
     `why_relevant` longtext NOT NULL,
     `preferred_regions` longtext NOT NULL,
     `approved` bool NOT NULL DEFAULT FALSE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE `websites_websitesubmission` ADD CONSTRAINT `websites_websitesubmission_submitter_id` FOREIGN KEY (`submitter_id`) REFERENCES `users` (`id`);
-ALTER TABLE `websites_websitesubmission` ADD CONSTRAINT `websites_websitesubmission_name` FOREIGN KEY (`name`) REFERENCES `translations` (`id`);
-ALTER TABLE `websites_websitesubmission` ADD CONSTRAINT `websites_websitesubmission_description` FOREIGN KEY (`description`) REFERENCES `translations` (`id`);
+ALTER TABLE `websites_websitesubmission` ADD CONSTRAINT `websites_websitesubmission__submitter_id` FOREIGN KEY (`submitter_id`) REFERENCES `users` (`id`);
+ALTER TABLE `websites_websitesubmission` ADD CONSTRAINT `websites_websitesubmission__name` FOREIGN KEY (`name`) REFERENCES `translations` (`id`);
+ALTER TABLE `websites_websitesubmission` ADD CONSTRAINT `websites_websitesubmission__description` FOREIGN KEY (`description`) REFERENCES `translations` (`id`);
 
-ALTER TABLE `websites_websitesubmission_keywords` ADD CONSTRAINT `websites_websitesubmission_websitesubmission_id` FOREIGN KEY (`websitesubmission_id`) REFERENCES `websites_websitesubmission` (`id`);
+ALTER TABLE `websites_websitesubmission_keywords` ADD CONSTRAINT `websites_websitesubmission__websitesubmission_id` FOREIGN KEY (`websitesubmission_id`) REFERENCES `websites_websitesubmission` (`id`);
