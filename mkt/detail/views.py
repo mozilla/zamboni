8d216c87cb70e242024176d29d1453dca7e9304f
Rob Hudson <robhudson@mozilla.com>
2015-06-25 15:38:15 -0700

    Reduce number of queries for packaged app manifest view (bug 1177530)

--- mkt/detail/views.py
+++ mkt/detail/views.py
@@ -1,5 +1,4 @@
 from django import http
-from django.shortcuts import get_object_or_404
 from django.views.decorators.http import etag
 
 import commonware.log
@@ -20,15 +19,23 @@ def manifest(request, uuid):
     If not a packaged app, returns a 404.
 
     """
-    addon = get_object_or_404(Webapp, guid=uuid, is_packaged=True)
-    is_avail = addon.status in [mkt.STATUS_PUBLIC, mkt.STATUS_UNLISTED,
-                                mkt.STATUS_BLOCKED]
-    is_owner = addon.authors.filter(pk=request.user.pk).exists()
-    is_owner_avail = addon.status == mkt.STATUS_APPROVED
+    try:
+        addon = (Webapp.objects.all().only_translations()
+                 .get(disabled_by_user=False, is_packaged=True, guid=uuid))
+    except Webapp.DoesNotExist:
+        raise http.Http404
 
-    if (addon.is_packaged and
-        not addon.disabled_by_user and
-            (is_avail or (is_owner_avail and is_owner))):
+    is_available = addon.status in [mkt.STATUS_PUBLIC, mkt.STATUS_UNLISTED,
+                                    mkt.STATUS_BLOCKED]
+    if is_available:
+        # If the package is available to anonymous users, we don't care about
+        # this and can save the query time.
+        is_owner = is_available_to_owner = False
+    else:
+        is_owner = addon.authors.filter(pk=request.user.pk).exists()
+        is_available_to_owner = addon.status == mkt.STATUS_APPROVED
+
+    if (is_available or (is_available_to_owner and is_owner)):
 
         manifest_content, manifest_etag = addon.get_cached_manifest()
 
