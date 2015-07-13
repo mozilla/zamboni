ecefee8c72f721938c00472ca830746c366078a8
Allen Short <ashort@mozilla.com>
2015-07-13 18:52:02 -0500

    give up on foreign key for now

--- migrations/922-leave-the-past-behind.sql
+++ migrations/922-leave-the-past-behind.sql
@@ -2,7 +2,6 @@
 -- o/` Leave the past behind, just walk away o/`
 
 ALTER TABLE django_admin_log DROP FOREIGN KEY `user_id_refs_id_c8665aa`;
-ALTER TABLE django_admin_log ADD FOREIGN KEY `django_admin_log_user_id_52fdd58701c5f563_fk_users_id` (`user_id`) REFERENCES `users` (`id`);
 DROP TABLE IF EXISTS auth_user_groups;
 DROP TABLE IF EXISTS auth_user_user_permissions;
 DROP TABLE IF EXISTS auth_user;
