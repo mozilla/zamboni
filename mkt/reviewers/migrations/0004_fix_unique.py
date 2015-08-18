6982e27821aa9bf9b193d35f7f047aaa9433d1a0
Allen Short <ashort@mozilla.com>
2015-08-18 11:23:14 -0500

    Fix incorrect constraint on tarako queue (bug 1195292)

--- mkt/reviewers/migrations/0004_fix_unique.py
+++ mkt/reviewers/migrations/0004_fix_unique.py
@@ -10,4 +10,6 @@ class Migration(migrations.Migration):
         ('reviewers', '0003_auto_20150727_1017'),
     ]
 
+    # An incorrect constraint was added in this migration, and was removed due
+    # to bug 1195292.
     operations = []
