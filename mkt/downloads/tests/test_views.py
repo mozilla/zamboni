d53e36ae95ae45a9941b6e5d6bb3e9c575a36715
Rob Hudson <robhudson@mozilla.com>
2015-08-21 14:07:34 -0700

    Fix mkt/downloads/ tests

--- mkt/downloads/tests/test_views.py
+++ mkt/downloads/tests/test_views.py
@@ -23,6 +23,9 @@ class TestDownload(BasePackagedAppTest):
         super(TestDownload, self).setup_files()
         self.url = reverse('downloads.file', args=[self.file.pk])
 
+    @override_settings(
+        DEFAULT_FILE_STORAGE='mkt.site.storage_utils.S3BotoPrivateStorage'
+    )
     @mock.patch.object(packaged, 'sign', mock_sign)
     def test_download(self):
         res = self.client.get(self.url)
@@ -66,6 +69,9 @@ class TestDownload(BasePackagedAppTest):
         self.file.update(status=mkt.STATUS_PENDING)
         eq_(self.client.get(self.url).status_code, 404)
 
+    @override_settings(
+        DEFAULT_FILE_STORAGE='mkt.site.storage_utils.S3BotoPrivateStorage'
+    )
     @mock.patch('lib.crypto.packaged.sign')
     def test_not_public_but_owner(self, sign):
         self.login('steamcube@mozilla.com')
@@ -81,6 +87,9 @@ class TestDownload(BasePackagedAppTest):
         self.file.update(status=mkt.STATUS_PENDING)
         eq_(self.client.get(self.url).status_code, 404)
 
+    @override_settings(
+        DEFAULT_FILE_STORAGE='mkt.site.storage_utils.S3BotoPrivateStorage'
+    )
     @mock.patch.object(packaged, 'sign', mock_sign)
     def test_disabled_but_owner(self):
         self.login('steamcube@mozilla.com')
@@ -93,6 +102,9 @@ class TestDownload(BasePackagedAppTest):
         res = self.client.get(self.url)
         self.assert3xx(res, path)
 
+    @override_settings(
+        DEFAULT_FILE_STORAGE='mkt.site.storage_utils.S3BotoPrivateStorage'
+    )
     @mock.patch.object(packaged, 'sign', mock_sign)
     def test_disabled_but_admin(self):
         self.login('admin@mozilla.com')
