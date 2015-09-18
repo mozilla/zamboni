mozilla/zamboni

commit abf6ba7cf8f31e9e24b14eea6d9c4b7da69ad276

Add information about developer agreement signatory status to FXA login view.

mkt/account/views.py
@@ -37,7 +37,7 @@ from mkt.account.serializers import (AccountSerializer, FeedbackSerializer,
                                      FxALoginSerializer, GroupsSerializer,
                                      LoginSerializer,
                                      NewsletterSerializer,
-                                     PermissionsSerializer)
+                                     PermissionsSerializer, TOSSerializer)
 from mkt.api.authentication import (RestAnonymousAuthentication,
                                     RestOAuthAuthentication,
                                     RestSharedSecretAuthentication)
@@ -290,6 +290,10 @@ class FxALoginView(CORSMixin, CreateAPIViewWithoutModel):
         permissions = PermissionsSerializer(context={'request': request},
                                             instance=True)
         data.update(permissions.data)
+
+        data.update({
+            'tos': TOSSerializer(context={'request': request}, instance=True)
+        })
 
         # Add ids of installed/purchased/developed apps.
         data['apps'] = user_relevant_apps(profile)

mkt/account/serializers.py
@@ -1,4 +1,7 @@
 from functools import partial
+from os import path
+
+from django.conf import settings
 
 from rest_framework import fields, serializers
 
@@ -158,3 +161,20 @@ class GroupsSerializer(serializers.ModelSerializer):
         model = Group
         fields = ('id', 'name', 'restricted')
         read_only_fields = ('id', 'name', 'restricted')
+
+
+class TOSSerializer(serializers.Serializer):
+    has_signed = fields.SerializerMethodField('get_has_signed')
+    url = fields.SerializerMethodField('get_url')
+
+    def get_has_signed(self, obj):
+        return (self.context['request'].user.read_dev_agreement is not None)
+
+    def get_url(self, obj):
+        accept_lang = self.context['request'].META['ACCEPT_LANGUAGE']
+        if path.exists(path.join(settings.MEDIA_ROOT, 'docs', 'terms',
+                       '%s.html' % accept_lang)):
+            lang = accept_lang
+        else:
+            lang = 'en-US'
+        return path.join(settings.MEDIA_URL, 'docs', 'terms', '%s.html' % lang)

mkt/account/tests/test_serializers.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 import mock
-from nose.tools import eq_
+from nose.tools import eq_, ok_
 
 import mkt
 import mkt.site.tests
-from mkt.account.serializers import AccountSerializer, AccountInfoSerializer
+from mkt.account.serializers import (AccountSerializer, AccountInfoSerializer,
+                                     TOSSerializer)
 from mkt.users.models import UserProfile
 
 
@@ -77,3 +80,25 @@ class TestAccountInfoSerializer(mkt.site.tests.TestCase):
     def test_verified(self):
         self.account.is_verified = True
         eq_(self.serializer().data['verified'], True)
+
+
+class TestTOSSerializer(mkt.site.tests.TestCase):
+    def setUp(self):
+        self.account = UserProfile()
+
+    def serializer(self, lang='pt-BR'):
+        context = {
+            'request': mkt.site.tests.req_factory_factory('')
+        }
+        context['request'].META['ACCEPT_LANGUAGE'] = lang
+        context['request'].user = self.account
+        return TOSSerializer(instance=self.account, context=context)
+
+    def test_has_signed(self):
+        eq_(self.serializer().data['has_signed'], False)
+        self.account.read_dev_agreement = datetime.now()
+        eq_(self.serializer().data['has_signed'], True)
+
+    def test_tos_url(self):
+        ok_('pt-BR' in self.serializer(lang='pt-BR').data['url'])
+        ok_('en-US' in self.serializer(lang='foo-LANG').data['url'])

commit 068f00e9432c77981d30d6d1be21962c5ca3e9d4

I broke login on dev in abf6ba7cf; this should fix it.

mkt/account/views.py
@@ -284,17 +284,17 @@ class FxALoginView(CORSMixin, CreateAPIViewWithoutModel):
                 'source': 'firefox-accounts',
             }
         }
+
+        context = {'request': request}
+
         # Serializers give up if they aren't passed an instance, so we
         # do that here despite PermissionsSerializer not needing one
         # really.
-        context ={'request': request}
         permissions = PermissionsSerializer(context=context, instance=True)
         data.update(permissions.data)
-        data['tos'] = TOSSerializer(context=context, instance=True).data
 
-        data.update({
-            'tos': TOSSerializer(context={'request': request}, instance=True)
-        })
+        # Has the user signed the developer agreement?
+        data['tos'] = TOSSerializer(context=context, instance=True).data
 
         # Add ids of installed/purchased/developed apps.
         data['apps'] = user_relevant_apps(profile)