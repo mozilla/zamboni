7572d3528db95b6a0ac901a8a497da213c6ba714
Kevin Ngo <me@ngokevin.com>
2015-11-30 13:46:34 -0800

    iarc v2 client (bug 1228376, 1228374)

---
+++ lib/iarc_v2/client.py
@@ -0,0 +1,71 @@
+# -*- coding: utf8 -*-
+import datetime
+import json
+import requests
+from urlparse import urljoin
+
+from django.conf import settings
+
+import commonware.log
+
+
+log = commonware.log.getLogger('z.iarc.v2')
+
+
+def get_rating_changes():
+    """
+    GetRatingChange for all changes within the last day.
+
+    TODO: Could add support for pagination, but very low priority since we
+          will never ever get anywhere close to 500 rating changes in a single
+          day.
+    """
+    if settings.IARC_V2_MOCK:
+        return json.loads(file('./mock/GetRatingChanges.json').read())
+    else:
+        start_date = datetime.datetime.utcnow()
+        end_date = start_date - datetime.timedelta(days=1)
+        url = urljoin(settings.IARC_V2_SERVICE_ENDPOINT, 'GetRatingChanges', {
+            'StartDate': start_date.strftime('%Y-%m-%d'),
+            'EndDate': end_date.strftime('%Y-%m-%d'),
+            'MaxRows': 500,  # Limit.
+            'StartRowIndex': 0  # Offset.
+        })
+        return requests.get(url).json()
+
+
+def search_certs(uuid):
+    """SearchCerts for complete data on a single certificate."""
+    if settings.IARC_V2_MOCK:
+        return json.loads(file('./mock/SearchCerts.json').read())
+    else:
+        url = urljoin(settings.IARC_V2_SERVICE_ENDPOINT, 'SearchCerts')
+        return requests.get(url, {'CertID': uuid}).json()
+
+
+def update_certs(cert_uuid, action):
+    """
+    UpdateCerts to tell IARC when we publish or unpublish a product.
+    Endpoint can handle batch updates, but we only need one at a time.
+
+    Arguments:
+    cert_uuid -- Globally unique ID for certificate.
+    action -- One of [InvalidateCert, RemoveProduct, UpdateStoreAttributes,
+                      Publish].
+
+    Return:
+    Update object.
+        ResultCode (string) -- Success or Failure
+        ErrorId (string) -- Can pass on to IARC for debugging.
+        ErrorMessage (string) -- Human-readable error message
+    """
+    if settings.IARC_V2_MOCK:
+        return json.loads(file('./mock/UpdateCerts.json').read())
+    else:
+        url = urljoin(settings.IARC_V2_SERVICE_ENDPOINT, 'UpdateCerts', {
+            'UpdateList': [{
+                'CertID': cert_uuid,
+                'Action': action
+            }]
+        })
+        return requests.post(url).json()
---
+++ lib/iarc_v2/tests/test_client.py
@@ -0,0 +1,58 @@
+import json
+import os
+
+import mock
+from django.conf import settings
+from django.test import TestCase
+from nose.tools import eq_, ok_
+
+from lib.iarc_v2.client import get_rating_changes, search_certs, update_certs
+
+
+mock_root = os.path.join(settings.ROOT, 'lib', 'iarc_v2', 'mock')
+
+responses = {
+    'GetRatingChanges': file(
+        os.path.join(mock_root, 'GetRatingChanges.json')
+    ).read(),
+    'SearchCerts': file(
+        os.path.join(mock_root, 'SearchCerts.json')
+    ).read(),
+    'UpdateCerts': file(
+        os.path.join(mock_root, 'UpdateCerts.json')
+    ).read()
+}
+
+
+def _get_response(service):
+    class Response(object):
+        def json(self):
+            return json.loads(responses[service])
+    return Response()
+
+
+class TestGetRatingChanges(TestCase):
+
+    @mock.patch('lib.iarc_v2.client.requests.get')
+    def test_basic(self, req_mock):
+        req_mock.return_value = _get_response('GetRatingChanges')
+        res = get_rating_changes()
+        eq_(res['Result']['ResponseCode'], 'Success')
+
+
+class TestSearchCerts(TestCase):
+
+    @mock.patch('lib.iarc_v2.client.requests.get')
+    def test_basic(self, req_mock):
+        req_mock.return_value = _get_response('SearchCerts')
+        res = search_certs('abc')
+        ok_(res['MatchFound'])
+
+
+class TestUpdateCerts(TestCase):
+
+    @mock.patch('lib.iarc_v2.client.requests.post')
+    def test_basic(self, req_mock):
+        req_mock.return_value = _get_response('UpdateCerts')
+        res = update_certs('abc', 'action')
+        eq_(res['ResultList'][0]['ResultCode'], 'Success')
