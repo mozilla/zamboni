51fdd6d5370f9636ab4115aee775029b5afa3d62
Allen Short <ashort@mozilla.com>
2016-01-21 07:37:54 -0600

    Memory profiler

--- mkt/zadmin/urls.py
+++ mkt/zadmin/urls.py
@@ -49,4 +49,6 @@ urlpatterns = patterns(
     url('^elastic$', views.elastic, name='zadmin.elastic'),
     url('^manifest-revalidation$', views.manifest_revalidation,
         name='zadmin.manifest_revalidation'),
+    url('^memory_profile$', views.memory_profile,
+        name='zadmin.memory_profile'),
 )
