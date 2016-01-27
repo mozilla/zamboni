3069221420ad537c0dbb81d1e7ef41708e7285ce
Allen Short <ashort@mozilla.com>
2016-01-26 19:05:58 -0600

    Revert "Memory profiler"
    This reverts commit 51fdd6d5370f9636ab4115aee775029b5afa3d62.

--- mkt/zadmin/urls.py
+++ mkt/zadmin/urls.py
@@ -49,6 +49,4 @@ urlpatterns = patterns(
     url('^elastic$', views.elastic, name='zadmin.elastic'),
     url('^manifest-revalidation$', views.manifest_revalidation,
         name='zadmin.manifest_revalidation'),
-    url('^memory_profile$', views.memory_profile,
-        name='zadmin.memory_profile'),
 )
